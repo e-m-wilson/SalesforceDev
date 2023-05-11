fd5b38f5b994cf4e5a4e56232fa24f3677fc9e47
Ethan Wilson <ethanwilson1991@gmail.com>
2023-05-11 12:34:48 -0400

    finished hamburger menu example

---
+++ force-app/main/default/lwc/buttonMenuHamburgerNavDemo/buttonMenuHamburgerNavDemo.js
@@ -0,0 +1,59 @@
+import { LightningElement } from 'lwc';
+
+export default class ButtonMenuDemo extends LightningElement {
+
+    Home = true;
+    CoolPage = false;
+    AboutUs = false;
+    smallFormFactor = true;
+    desktopFormFactor = false;
+
+    menuChange(e) {
+        let choice = e.target.dataset.menu;
+        switch(choice) {
+            case 'Home':
+            {
+                this.Home = true;
+                this.CoolPage = false;
+                this.AboutUs = false;
+                break;
+            }
+            case 'Cool Page':
+            {
+                this.Home = false;
+                this.CoolPage = true;
+                this.AboutUs = false;
+                break;
+            }
+            case 'About Us':
+            {
+                this.Home = false;
+                this.CoolPage = false;
+                this.AboutUs = true;
+                break;
+            }
+            default:
+            {
+                this.Home = true;
+                this.CoolPage = false;
+                this.AboutUs = false;
+            }
+        }
+    }
+
+    connectedCallback() {
+        window.addEventListener('resize', this.handleWindowChange);
+    }
+        
+    handleWindowChange = () => {
+        let width = this.template.querySelector('.header').getBoundingClientRect().width;
+        console.log(width);
+        if(width < 800){
+            this.smallFormFactor = true;
+            this.desktopFormFactor = false;
+        } else {
+            this.smallFormFactor = false;
+            this.desktopFormFactor = true;
+        }
+    }
+}
