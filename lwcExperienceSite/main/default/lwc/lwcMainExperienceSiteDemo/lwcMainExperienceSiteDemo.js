0b4e62fb06297647bf81850551e9a57e5d940ffd
Ethan Wilson <ethanwilson1991@gmail.com>
2023-05-18 16:18:25 -0400

    styling update

--- lwcExperienceSite/main/default/lwc/lwcMainExperienceSiteDemo/lwcMainExperienceSiteDemo.js
+++ lwcExperienceSite/main/default/lwc/lwcMainExperienceSiteDemo/lwcMainExperienceSiteDemo.js
@@ -6,9 +6,6 @@ export default class LwcMainExperienceSiteDemo extends LightningElement {
     Home = true;
     CoolPage = false;
     AboutUs = false;
-    smallFormFactor = true;
-    desktopFormFactor = false;
-
     
     firstName;
     logoutURL;
@@ -44,11 +41,15 @@ export default class LwcMainExperienceSiteDemo extends LightningElement {
                 this.AboutUs = false;
             }
         }
+    }
+
+    mobileMenuChange(e) {
+        this.menuChange(e);
         this.toggleMenu();
     }
 
     connectedCallback() {
-        window.addEventListener('resize', this.handleWindowChange);
+     
         /*Note that the logout will not work in the experience builder.
         It will only work in a live and published experience site. */
         this.logoutURL = window.location.origin + '/secur/logout.jsp';
@@ -61,28 +62,10 @@ export default class LwcMainExperienceSiteDemo extends LightningElement {
         });
     }
 
-    renderedCallback() {
-        this.handleWindowChange();
-    }
-        
-    handleWindowChange = () => {
-        let width = this.template.querySelector('.header').getBoundingClientRect().width;
-        console.log(width);
-        if(width < 800){
-            this.smallFormFactor = true;
-            this.desktopFormFactor = false;
-        } else {
-            this.smallFormFactor = false;
-            this.desktopFormFactor = true;
-            this.toggleMenu();
-        }
-    }
+  
+   
 
     toggleMenu() {
-        if(this.smallFormFactor) {
-           this.template.querySelector('.mainMenu').classList.toggle('active'); 
-        } else {
-            this.template.querySelector('.mainMenu').classList.remove('active');
-        }
+        this.template.querySelector('.mobileMenu').classList.toggle('active');
     }
 }
