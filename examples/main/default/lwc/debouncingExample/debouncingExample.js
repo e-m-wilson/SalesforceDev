ccd676fcfb0f908bc5ab24356ba0ea04fce30b30
Ethan Wilson <ethanwilson1991@gmail.com>
2024-06-25 16:26:03 -0500

    updates...

---
+++ examples/main/default/lwc/debouncingExample/debouncingExample.js
@@ -0,0 +1,33 @@
+import { LightningElement, wire } from 'lwc';
+import getAccountList from '@salesforce/apex/AccountHelper.getAccountList';
+
+export default class DebouncingExample extends LightningElement {
+
+    myQuery;
+
+    @wire(getAccountList, {query : '$myQuery'})
+    allAccounts;
+
+    columns = [
+        { label: 'Name', fieldName: 'Name', type:'name' },
+        { label: 'Annual Revenue', fieldName: 'AnnualRevenue', type: 'currency' }
+    ];
+
+    handleQuery(e){
+        let q = e.target.value;
+
+        setTimeout( () => {
+           this.myQuery = q; 
+        }, 2000);
+
+        /*
+            This will not work. e.target.value needs placed inside a function scope 
+            variable - then you can use the variable
+
+           setTimeout( () => {
+           this.myQuery = e.target.value; 
+        }, 2000);
+        */
+        
+    }
+}
--- examples/main/default/lwc/trackDecoratorExample/trackDecoratorExample.js
+++ examples/main/default/lwc/trackDecoratorExample/trackDecoratorExample.js
@@ -1,12 +1,17 @@
 import { LightningElement, track } from 'lwc';
 
 export default class TrackDecoratorExample extends LightningElement {
-    @track fruits = ['Apple', 'Banana', 'Cherry'];
+    fruits = ['Apple', 'Banana', 'Cherry'];
     vegetables = ['Lettuce', 'Asparagus', 'Cabbage'];
+
+    myVariable = {message: 'hello world'};
 
     handleButtonClick() {
         // This will trigger a re-render, because we're modifying an @track property
         this.fruits.push('Date');
         this.vegetables.push('Broccoli');
+        this.myVariable.message = 'hello mars';
     }
+
+    
 }
