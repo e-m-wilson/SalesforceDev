72ca2dd2e4e1da6063256f47bb1c8510fc19171b
Ethan Wilson <ethanwilson1991@gmail.com>
2025-02-05 16:03:53 -0600

    added comments

--- examples/main/default/lwc/getListRecordsByName/getListRecordsByName.js
+++ examples/main/default/lwc/getListRecordsByName/getListRecordsByName.js
@@ -16,11 +16,16 @@ export default class GetListRecordsByName extends LightningElement {
     // only objectApiName and listViewName are required
     @wire(getListRecordsByName, {
         objectApiName: ACCOUNT_OBJECT.objectApiName,
+        // api name of a list view, you cant get it from the URL when looking at a list view
         listViewApiName: "AllAccounts",
+        // these need to very specifically be ObjectApiName.FieldName
         fields: ["Account.Name", "Account.AnnualRevenue"],
+        // the search term supports wildcards and is the best option over filtering by name with the where graphQL attribute
         searchTerm: "$query",
+        // "-" is specifying a descending order. leave it off for ascending
         sortBy: ['-Account.AnnualRevenue'],
         // where uses GraphQL syntax:
+        // only eq is working as of now 2/5/24, but eq works and nothing else throws errors 
         where: {and: [{ Rating: {eq: 'Hot'} }, { AnnualRevenue: {gt: 0}}, { AnnualRevenue: {ne: null} }]}
     })accounts({data, error}) {
         if(data) {
