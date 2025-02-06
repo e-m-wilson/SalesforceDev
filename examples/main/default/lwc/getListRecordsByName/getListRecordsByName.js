48244724ec56c83a23a1bf9734198c5dfc071744
Ethan Wilson <ethanwilson1991@gmail.com>
2025-02-05 18:56:38 -0600

    update to getListRecordsByName

--- examples/main/default/lwc/getListRecordsByName/getListRecordsByName.js
+++ examples/main/default/lwc/getListRecordsByName/getListRecordsByName.js
@@ -1,18 +1,16 @@
 import { LightningElement, wire } from 'lwc';
 import { getListRecordsByName } from "lightning/uiListsApi";
 import ACCOUNT_OBJECT from '@salesforce/schema/Account';
-import ACCOUNT_NAME from '@salesforce/schema/Account.Name';
-import ACCOUNT_ANNUAL_REVENUE from '@salesforce/schema/Account.AnnualRevenue';
 
 export default class GetListRecordsByName extends LightningElement {
 
     error;
     query;
-    accounts;
-    
 
     // WARNING!!!
-    // USE PROPERTY SYNTAX TO AVOID ISSUES
+    // PROPERTY SYNTAX NEEDS SOME SPECIFIC SETUP
+    // -- Need to have a getter as below: get accList() 
+    // -- Need to use lwc:if on accounts.data first before trying to loop through accList
     // only objectApiName and listViewName are required
     @wire(getListRecordsByName, {
         objectApiName: ACCOUNT_OBJECT.objectApiName,
@@ -27,15 +25,13 @@ export default class GetListRecordsByName extends LightningElement {
         // where uses GraphQL syntax:
         // valid field operators: https://developer.salesforce.com/docs/platform/graphql/guide/filter-fields.html#field-functions
         where: "{and: [{ Rating: {eq: 'Hot'} }, { AnnualRevenue: {gt: 0}}, { AnnualRevenue: {ne: null} }]}"
-    })accounts({data, error}) {
-        if(data) {
-            this.accounts = data.records;
-        } else if(error) {
-            this.error = error;
-        }
-    };
+    })accounts;
 
+    get accList() {
+        return this.accounts.data.records;
+    }
     search() {
         this.query = this.refs.input.value;
     }
+
 } 
